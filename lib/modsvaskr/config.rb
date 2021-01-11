5dd623965fd82090cdf72e42c2ffe9d473f6e53a
Muriel Salvan <muriel@x-aeon.com>
2021-01-11 12:43:53 +0100

    Add checks with user-friendly errors when config references an unknown game type

--- lib/modsvaskr/config.rb
+++ lib/modsvaskr/config.rb
@@ -12,7 +12,19 @@ module Modsvaskr
     # Parameters::
     # * *file* (String): File containing configuration
     def initialize(file)
-      @config = YAML.load(File.read(file))
+      @config = YAML.load(File.read(file)) || {}
+      # Parse all game types plugins
+      # Hash<Symbol, Class>
+      @game_types = Hash[
+        Dir.glob("#{__dir__}/games/*.rb").map do |game_type_file|
+          require game_type_file
+          base_name = File.basename(game_type_file, '.rb')
+          [
+            base_name.to_sym,
+            Games.const_get(base_name.split('_').collect(&:capitalize).join.to_sym)
+          ]
+        end
+      ]
     end
 
     # Get the games list
@@ -21,9 +33,10 @@ module Modsvaskr
     # * Array<Game>: List of games
     def games
       unless defined?(@games)
-        @games = @config['games'].map do |game_info|
-          require "#{__dir__}/games/#{game_info['type']}.rb"
-          Games.const_get(game_info['type'].to_s.split('_').collect(&:capitalize).join.to_sym).new(self, game_info)
+        @games = (@config['games'] || []).map do |game_info|
+          game_type = game_info['type'].to_sym
+          raise "Unknown game type: #{game_type}. Available ones are #{@game_types.keys.join(', ')}" unless @game_types.key?(game_type)
+          @game_types[game_type].new(self, game_info)
         end
       end
       @games
