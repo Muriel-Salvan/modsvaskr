d4badaf183b36edeb2cd367b9d67a667098bdfce
Muriel Salvan <muriel@x-aeon.com>
2021-08-18 12:47:45 +0200

    Add UTF-8 test in game title

--- spec/tests/main_menu_spec.rb
+++ spec/tests/main_menu_spec.rb
@@ -59,6 +59,44 @@ describe 'Main menu' do
     expect_menu_items_to_include('Test Game 3')
   end
 
+  it 'displays several games using UTF-8' do
+    run_modsvaskr(
+      config: {
+        'games' => [
+          {
+            'name' => 'Test Game 1 - 素晴らしいゲーム',
+            'path' => '/path/to/test_game_1',
+            'type' => 'test_game',
+            'launch_exe' => 'game_launcher.exe'
+          },
+          {
+            'name' => 'Test Game 2 - さらに良いゲーム',
+            'path' => '/path/to/test_game_2',
+            'type' => 'test_game',
+            'launch_exe' => 'game_launcher.exe'
+          },
+          {
+            'name' => 'Test Game 3',
+            'path' => '/path/to/test_game_3 - それほど素晴らしいゲームではありません',
+            'type' => 'test_game',
+            'launch_exe' => 'game_launcher.exe'
+          }
+        ]
+      }
+    )
+    # Ruby curses current does not support UTF-8 correctly in the inch method.
+    # We can still check that it works with TEST_DEBUG=1
+    # cf https://github.com/ruby/curses/issues/65
+    # TODO: Uncomment when Ruby curses will be fixed.
+    # rubocop:disable Style/AsciiComments
+    # expect_menu_items_to_include('Test Game 1 - 素晴らしいゲーム')
+    # expect_menu_items_to_include('Test Game 2 - さらに良いゲーム')
+    # expect_menu_items_to_include('Test Game 3 - それほど素晴らしいゲームではありません')
+    # rubocop:enable Style/AsciiComments
+    expect_menu_items_to_include(/Test Game 1 - .+/)
+    expect_menu_items_to_include(/Test Game 2 - .+/)
+  end
+
   it 'initializes the game plugin when used' do
     initialized = false
     ModsvaskrTest::Games::TestGame.init_proc = proc do
